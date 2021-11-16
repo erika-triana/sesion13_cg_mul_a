6abbce24555b4e8f857fb89921519a91c4c29ce5
erika-triana <est.erika.triana@unimilitar.edu.co>
2021-11-16 00:01:55 -0500

    Signed-off-by: erika-triana <est.erika.triana@unimilitar.edu.co>

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,103 +1,89 @@
 var scene = new THREE.Scene();
 
-function cubo(x, y, z, color, material, alambrado)
-{
+function cubo(x, y, z, r,g,b, material, alambrado)
+ {
     var cubeGeometry = new THREE.BoxGeometry(x, y, z);
     var cubeMaterial;
-    switch(material)
+    switch (material) 
     {
-     case 'Basic': cubeMaterial = new THREE.MeshBasicMaterial({color: color, wireframe: alambrado});
-      break;
+        case 'Physical': cubeMaterial = new THREE.MeshPhysicalMaterial({wireframe: alambrado });
+                         cubeMaterial.color.setRGB(r,g,b);
+            break;
 
-     case 'Standard': cubeMaterial = new THREE.MeshStandardMaterial({color: color, wireframe: alambrado});
-      break;
+        case 'Basic': cubeMaterial = new THREE.MeshBasicMaterial({ wireframe: alambrado });
+        cubeMaterial.color.setRGB(r,g,b);
+            break;
 
-     case 'Physical': cubeMaterial = new THREE.MeshPhysicalMaterial({color: color, wireframe: alambrado});
-      break;
+        case 'Standard': cubeMaterial = new THREE.MeshStandardMaterial({ wireframe: alambrado });
+        cubeMaterial.color.setRGB(r,g,b);
+            break;
 
-     case 'Phong': cubeMaterial = new THREE.MeshPhongMaterial({color: color, wireframe: alambrado});
-      break;
+        case 'Phong': cubeMaterial = new THREE.MeshPhongMaterial({ wireframe: alambrado });
+        cubeMaterial.color.setRGB(r,g,b);
+            break;
 
-     case 'Lambert': cubeMaterial = new THREE.MeshLambertMaterial({color: color, wireframe: alambrado});
-      break;
+        case 'Lambert': cubeMaterial = new THREE.MeshLambertMaterial({ wireframe: alambrado });
+        cubeMaterial.color.setRGB(r,g,b);
+                        
+            break;
     }
-    
+
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 
     scene.add(cube);
-    return(cube);
+    return (cube);
 }
-function init()
- {
 
+function init() 
+{
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0x000000));
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var axes = new THREE.AxesHelper(20);
+    var axes = new THREE.AxesHelper(50);
     scene.add(axes);
 
-   light = new THREE.PointLight(0xFFFF00); 
-    light.position.set( -10, 12, 10 );            
-    scene.add( light ); 
-
-
     Cubo = [];   
-    var dim = prompt("Ingrese valor correspondiente a la dimension del cubo ","Valor"); 
-    if(dim != null)
-    {
-	alert("Valor correspondiente a la dimension:" + dim); 
-    } else 
-    {
-	alert("No se registro ningun valor."); 
-    }
 
-    delta= dim/2;
-    diagonal= Math.sqrt(Math.pow(delta, 2)+ Math.pow(delta, 2));
-    
-    var Angulo = prompt("Ingrese en grados el valor correspondiente al angulo del cubo que desea rotar","Valor"); 
-    if(Angulo != null)
-    {
-	alert("Valor correspondiente al angulo:" + Angulo); 
-    } else 
-    {
-	alert("No se registro ningun valor."); 
-    }
-    ang_rad= (Angulo)*((2*Math.PI)/(360)); 
-    Angulo_2= ((Math.PI)/4)-ang_rad;
-    valor=(Math.cos(Angulo_2))*diagonal;
+    var dimension = prompt('Escriba el valor correspondiente a la dimension del cubo',5);
+    var dim1 = parseInt(dimension, 10);
 
-    Cubo.push(cubo(dim, dim, dim, 'green', 'Physical', false)); 
+    dim = dim1; 
+    dimscale = dim / dim; 
+    dim2 = dimscale / 2; 
+    dim3 = dimscale / 4;  
+    angulo=(Math.PI/6);
+    angulo1=(Math.PI/4)-angulo;
 
-    Cubo.push(cubo(dim, dim, dim, 'red', 'Physical', false)); 
+    delta = dim / 2; 
+    beta = dim + delta; 
+    alpha2 = delta * 2.5; 
+    alpha3 = delta * 3.25;
 
-    Cubo.push(cubo(dim, dim, dim, 'blue', 'Physical', false)); 
+    Cubo.push(cubo(dim, dim, dim, 0.55, 0.55, 0, 'Lambert', false));
+    Cubo.push(cubo(dim, dim, dim, 0.5, 1.8, 0, 'Lambert', false));
+    Cubo.push(cubo(dim, dim, dim, 0.5, 1.8, 0, 'Lambert', false));
 
-    for(i=0; i<3; i++)
-    {  
+    Cubo[0].translateX(delta); 
+    Cubo[0].translateY(delta);
+    Cubo[0].translateZ(delta);
 
-      Cubo[i].translateX(valor); 
-      Cubo[i].translateZ(valor); 
-      Cubo[i].translateY(delta); 
-    }
-    
-    for(i=1; i<3; i++)
-    { 
+    Cubo[1].translateX(delta); 
+    Cubo[1].translateY(alpha2); 
+    Cubo[1].translateZ(delta);
+    Cubo[1].scale.set(dim2, dim2, dim2); 
 
-        escala= 1/(2*i); 
-        unidades=dim/2+dim/4+((((dim/2)+(dim/4))/2))*(i-1);
-        Cubo[i].scale.set(escala, escala, escala); 
-        Cubo[i].translateY(unidades); 
+    Cubo[2].translateX(delta); 
+    Cubo[2].translateY(alpha3); 
+    Cubo[2].scale.set(dim3, dim3, dim3); 
 
-    }
+    light = new THREE.PointLight(0xFFFF00); 
+    light.position.set((dim/2)+dim, ((dim/3)*2)+dim*2, (dim*3)+dim*3); 
+    scene.add(light);
 
-    Cubo[0].rotateY(ang_rad);
-    Cubo[2].rotateY(ang_rad);
-
-
-    camera.position.set(-3*dim, 4*dim, 3*dim);
+    camera.position.set((dim/2)+dim, ((dim/3)*2)+dim*2, (dim*3)+dim*3);
     camera.lookAt(scene.position);
 
     document.getElementById("webgl-output").appendChild(renderer.domElement);
